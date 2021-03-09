8fa50ed0060d864f3549707dc02661a08da7fe67
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-09 01:32:50 -0300

    Feat: add http verbs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,37 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (request, response) => response.json({ message: 'Hello World' }));
+server.get('/courses', (request, response) => response.json([
+  'Curso 1',
+  'Curso 2',
+  'Curso 3',
+]));
+
+server.post('/courses', (request, response) => response.json([
+  'Curso 1',
+  'Curso 2',
+  'Curso 3',
+  'Curso 4',
+]));
+
+server.put('/courses/:id', (request, response) => response.json([
+  'Curso 99',
+  'Curso 2',
+  'Curso 3',
+  'Curso 4',
+]));
+
+server.patch('/courses/:id', (request, response) => response.json([
+  'Curso 99',
+  'Curso 02',
+  'Curso 3',
+  'Curso 4',
+]));
+
+server.delete('/courses/:id', (request, response) => response.json([
+  'Curso 99',
+  'Curso 02',
+  'Curso 4',
+]));
 
 server.listen(3030);
