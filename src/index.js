e6c2e2dce87012d3c98ff00d44d39d9c11429b35
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-09 00:55:07 -0300

    Feat: get simple response

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' })
+});
+
 server.listen(3030);
