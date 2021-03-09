74d9c2b2ca59a2a37c1874d8e3fa0fb02ad585ad
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-09 17:16:13 -0300

    Feat: add route and middleware to withdraw in statement

--- src/index.js
+++ src/index.js
@@ -21,6 +21,17 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((total, operation) => {
+    if (operation.type === 'credit') {
+      return total + operation.amount;
+    }
+    return total - operation.amount;
+  }, 0);
+
+  return balance;
+}
+
 server.post('/account', (request, response) => {
   const { name, cpf } = request.body;
 
@@ -55,6 +66,27 @@ server.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
     amount,
     created_at: new Date(),
     type: 'credit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+server.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds' });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit',
   };
 
   customer.statement.push(statementOperation);
