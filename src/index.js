d1c101de43e4c58172b13e5ab8933f1e1ea9d6a3
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-09 01:47:22 -0300

    Feat: explore route, query and body params

--- src/index.js
+++ src/index.js
@@ -2,25 +2,45 @@ const express = require('express');
 
 const server = express();
 
-server.get('/courses', (request, response) => response.json([
-  'Curso 1',
-  'Curso 2',
-  'Curso 3',
-]));
+server.use(express.json());
 
-server.post('/courses', (request, response) => response.json([
-  'Curso 1',
-  'Curso 2',
-  'Curso 3',
-  'Curso 4',
-]));
+server.get('/courses', (request, response) => {
+  const { query } = request;
 
-server.put('/courses/:id', (request, response) => response.json([
-  'Curso 99',
-  'Curso 2',
-  'Curso 3',
-  'Curso 4',
-]));
+  console.log(query);
+
+  return response.json([
+    'Curso 1',
+    'Curso 2',
+    'Curso 3',
+  ]);
+});
+
+server.post('/courses', (request, response) => {
+  const { body } = request;
+
+  console.log(body);
+
+  return response.json([
+    'Curso 1',
+    'Curso 2',
+    'Curso 3',
+    'Curso 4',
+  ]);
+});
+
+server.put('/courses/:id', (request, response) => {
+  const { params } = request;
+
+  console.log(params);
+
+  return response.json([
+    'Curso 99',
+    'Curso 2',
+    'Curso 3',
+    'Curso 4',
+  ]);
+});
 
 server.patch('/courses/:id', (request, response) => response.json([
   'Curso 99',
