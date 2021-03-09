e8627d82ad0ee85808b16ca29e06119d4a7b2d04
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-09 16:58:24 -0300

    Feat: add route to deposit in statement

--- src/index.js
+++ src/index.js
@@ -45,4 +45,21 @@ server.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+server.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
 server.listen(3030);
